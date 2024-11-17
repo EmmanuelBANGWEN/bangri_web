293071580eb2932e055839609665908425e910a0
Emmanuel Bangwen <149902111+EmmanuelBANGWEN@users.noreply.github.com>
2024-11-17 18:01:41 +0100

    Update settings.py
    true to false debug

--- monsite/settings.py
+++ monsite/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y$xq3tg(-pqfgp7im-62lh1*-i@kt87e%pwhe6)w3^u=kz5^xe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
